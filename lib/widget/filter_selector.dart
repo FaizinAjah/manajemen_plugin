cc461f7641d293db4a21feeb6b9770b2c6a1aba3
FaizinAjah <faizinajah13@gmail.com>
2024-11-04 12:55:07 +0700

    Initial commit

---
+++ lib/widget/filter_selector.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:manajemen_plugin/widget/filter_item.dart';
+
+
+@immutable
+class FilterSelector extends StatefulWidget {
+  const FilterSelector({
+    super.key,
+    required this.filters,
+    required this.onFilterChanged,
+    this.padding = const EdgeInsets.symmetric(vertical: 24),
+  });
+
+  final List<Color> filters;
+  final void Function(Color selectedColor) onFilterChanged;
+  final EdgeInsets padding;
+
+  @override
+  State<FilterSelector> createState() => _FilterSelectorState();
+}
+
+class _FilterSelectorState extends State<FilterSelector> {
+  late final PageController _controller;
+  late int _page;
+
+  int get filterCount => widget.filters.length;
+
+  Color itemColor(int index) => widget.filters[index % filterCount];
+
+  @override
+  void initState() {
+    super.initState();
+    _page = 0;
+    _controller = PageController(
+      initialPage: _page,
+      viewportFraction: 0.3, 
+    );
+    _controller.addListener(_onPageChanged);
+  }
+
+  void _onPageChanged() {
+    final page = (_controller.page ?? 0).round();
+    if (page != _page) {
+      _page = page;
+      widget.onFilterChanged(widget.filters[page]);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.transparent,
+            Colors.black,
+          ],
+        ),
+      ),
+      child: SizedBox(
+        height: 100, 
+        child: _buildCarousel(),
+      ),
+    );
+  }
+
+  Widget _buildCarousel() {
+    return PageView.builder(
+      controller: _controller,
+      itemCount: filterCount,
+      itemBuilder: (context, index) {
+        return Container(
+          margin: EdgeInsets.symmetric(horizontal: 8), 
+          child: FilterItem(
+            onFilterSelected: () => _onFilterTapped(index),
+            color: itemColor(index),
+          ),
+        );
+      },
+    );
+  }
+
+  void _onFilterTapped(int index) {
+    widget.onFilterChanged(widget.filters[index]);
+  }
+
+  Widget _buildSelectionRing(double itemSize) {
+    return IgnorePointer(
+      child: Padding(
+        padding: widget.padding,
+        child: SizedBox(
+          width: itemSize,
+          height: itemSize,
+          child: const DecoratedBox(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.fromBorderSide(
+                BorderSide(width: 6, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
